02b8762615a791ae9ed12e9172206ab21c287ec0
Ann27Jiang <49375737+Ann27Jiang@users.noreply.github.com>
2019-11-14 10:35:00 -0500

    implementation of edit course

--- src/main/java/com/greenlab/greenlab/controller/course/EditCourseController.java
+++ src/main/java/com/greenlab/greenlab/controller/course/EditCourseController.java
@@ -1,10 +1,12 @@
 package com.greenlab.greenlab.controller.course;
 
+import com.greenlab.greenlab.model.Course;
 import com.greenlab.greenlab.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 // import org.springframework.web.bind.annotation.PostMapping;
@@ -17,10 +19,25 @@ public class EditCourseController{
     @Autowired
     private CourseRepository courseRepository;
     @GetMapping(value = "/course/edit")
-    public String getCreateCourse(ModelMap model, HttpServletRequest request) {
+    public String getCreateCourse(ModelMap model, @RequestParam(value = "course", required = false) String cou,
+                                  HttpServletRequest request) {
 //        if (request.getSession().getAttribute("email") == null)
 //            return "redirect:/login";
 //        Course course
+
+        return "profEditCourse";
+    }
+
+    @GetMapping("/profEditCourse")
+    public String profEditCourse(@RequestParam(value = "id") String id,
+                                 ModelMap model,
+                                 HttpServletRequest request) {
+//        if (request.getSession().getAttribute("email") == null)
+//            return "redirect:/login";
+        Course course = courseRepository.findByCourseId(id);
+
+        System.out.println(id);
+
         return "profEditCourse";
     }
 
