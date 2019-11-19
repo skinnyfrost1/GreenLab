e86dd65903aede4cc3402b2c3212f2b771f89a0c
Ann27Jiang <49375737+Ann27Jiang@users.noreply.github.com>
2019-11-19 01:56:04 -0500

    "Fix bug in sign up html"

--- src/main/java/com/greenlab/greenlab/controller/course/CreateCourseController.java
+++ src/main/java/com/greenlab/greenlab/controller/course/CreateCourseController.java
@@ -23,6 +23,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -108,5 +109,6 @@ public class CreateCourseController {
         return "redirect:/courses";
 
     }
+
 
 }
