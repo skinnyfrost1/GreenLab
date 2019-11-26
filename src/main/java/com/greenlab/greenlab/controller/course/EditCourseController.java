2d7fa8673e734b6d812a43361d7b1e061c95a763
Ann27Jiang <49375737+Ann27Jiang@users.noreply.github.com>
2019-11-26 15:44:05 -0500

    "Update of edit course"

--- src/main/java/com/greenlab/greenlab/controller/course/EditCourseController.java
+++ src/main/java/com/greenlab/greenlab/controller/course/EditCourseController.java
@@ -1,7 +1,9 @@
 package com.greenlab.greenlab.controller.course;
 
 import com.greenlab.greenlab.model.Course;
+import com.greenlab.greenlab.model.Lab;
 import com.greenlab.greenlab.repository.CourseRepository;
+import com.greenlab.greenlab.repository.LabRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -13,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 // import org.springframework.web.bind.annotation.PostMapping;
 // import org.springframework.web.bind.annotation.RequestBody;
 
@@ -22,16 +25,22 @@ public class EditCourseController{
 
     @Autowired
     private CourseRepository courseRepository;
+    @Autowired
+    private LabRepository labRepository;
+
     @GetMapping(value = "/course/edit")
     public String getCreateCourse(ModelMap model, @RequestParam(value = "id") String id,
                                   HttpServletRequest request) {
-//        if (request.getSession().getAttribute("email") == null)
-//            return "redirect:/login";
+        if (request.getSession().getAttribute("email") == null)
+            return "redirect:/login";
 //        Course course
+        String email = (String)request.getSession().getAttribute("email");
         Course course = courseRepository.findBy_id(id);
-        model.addAttribute("course",course);
-//        System.out.println(id);
 
+        List<Lab> labs = labRepository.findByCourseIdAndCreator(course.getCourseId(),email);
+//        System.out.println(id);
+        model.addAttribute("course",course);
+        model.addAttribute("labs",labs);
         return "profEditCourse";
     }
 
--- src/main/java/com/greenlab/greenlab/repository/LabRepository.java
+++ src/main/java/com/greenlab/greenlab/repository/LabRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 public interface LabRepository extends MongoRepository<Lab, String> {
     List<Lab> findByCourseId (String courseId);
     Lab findBy_id(String _id);
+    List<Lab> findByCourseIdAndCreator(String courseId, String creator);
 }
