6fbb89735b2758db46281d8a7118cb390563c565
YiweiRayZhao <45610116+YiweiRayZhao@users.noreply.github.com>
2019-12-18 01:13:46 -0500

    add css aj

--- src/main/resources/static/js/ProfViewSearchEquipments.js
+++ src/main/resources/static/js/ProfViewSearchEquipments.js
@@ -46,3 +46,35 @@ function GetCheckBoxListValues()
 
 
 }
+
+// $(document).ready(function () {
+//   var equipmentContainer = document.getElementById("equipmentsContainer");
+//   var posting = {};
+//   posting['str'] = "give me search equipments"
+//   $.ajax({
+//     type: "POST",
+//     contentType: "application/json",
+//     url: "/equipment/search",
+//     data: JSON.stringify(posting),
+//     dataType: 'json',
+//     cache: false,
+//
+//     timeout: 600000,
+//     success: function (result) {
+//       console.log(result);
+//       console.log(result.length)
+//       for (var i = 0; i < result.length; i++) {
+//         var _id = result[i]._id;
+//         var equipmentName = result[i].equipmentName;
+//         var data = result[i].image;
+//         var str = equipmentContainer.innerHTML;
+//         equipmentContainer.innerHTML = str + '<div class="equipment viewEquipContainer webfont" id="' + _id + '">' + equipmentName +
+//           '<img class="imageContainer" src = "data:image/png;base64,' + data + '">' + '</div>'
+//       }
+//     },
+//     error: function (e) {
+//
+//     }
+//   });
+//
+// });
