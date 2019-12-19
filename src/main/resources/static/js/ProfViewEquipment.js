27ab5c3821d84dd32ada97ab56d695e8226b180d
YiweiRayZhao <45610116+YiweiRayZhao@users.noreply.github.com>
2019-12-19 07:03:12 -0500

    view equip css

--- src/main/resources/static/js/ProfViewEquipment.js
+++ src/main/resources/static/js/ProfViewEquipment.js
@@ -20,8 +20,40 @@ $(document).ready(function () {
         var data = result[i].image;
         var str = equipmentContainer.innerHTML;
 
-        equipmentContainer.innerHTML = str + '<div class="equipment viewEquipContainer webfont" id="' + _id + '">' + equipmentName +
-          '<img class="imageContainer" src = "data:image/png;base64,' + data + '">' + '</div>'
+        // equipmentContainer.innerHTML = str + '<div class="equipment viewEquipContainer webfont" id="' + _id + '">' + equipmentName +
+        //   '<img class="imageContainer" src = "data:image/png;base64,' + data + '">' + '</div>'
+
+        equipmentContainer.innerHTML = str +
+            '                    <div class="bigLabContainer">\n' +
+            '                    <div class="ViewEquipmentContainer">\n' +
+            '                        <table class="labContainer" >\n' +
+            '                            <col width="30%"/>\n' +
+            '                            <col width="40%"/>\n' +
+            '                            <col width="30%"/>\n' +
+            '                            <!-- <col width="20%"/> -->\n' +
+            '                            <!-- <tbody style="padding: 0; margin: 0;"> -->\n' +
+            '                            <tr>\n' +
+            '                                <td class="courseCellPadding">\n' +
+            '                                    <div class="equipment">\n' +
+            '                                       <img src = "data:image/png;base64,' + data + '" style="width:80px;height:80px">' +
+            '                                    </div>\n' +
+            '                                </td>\n' +
+            '                                <td class="courseCellPadding">\n' +
+            '                                    <div class="webfont" >' + equipmentName + '</div>\n' +
+            '                                </td>\n' +
+            '                                <td class="courseCellPadding">\n' +
+            '                                    <!--                            <div class="checkBoxDiv">-->\n' +
+            '                                    <input class="webfont" name="createLabEquipSelected" type="checkbox" id="' + _id + '"  style="width:20px;height:20px"></input>\n' +
+            '                                    <!--                            </div>-->\n' +
+            '                                </td>\n' +
+            '                            </tr>\n' +
+            '                        </table>\n' +
+            '                    </div>\n' +
+            '                    </div>'
+
+
+
+
 
       }
     },
